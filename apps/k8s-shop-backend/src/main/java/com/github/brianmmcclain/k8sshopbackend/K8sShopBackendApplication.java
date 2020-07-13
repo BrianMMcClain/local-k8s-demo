fc6b09c6aa944587e0cbffd500341a9b0972bbf3
Brian McClain <brianmmcclain@gmail.com>
2020-07-13 15:47:58 -0400

    Remove debugging output

--- apps/k8s-shop-backend/src/main/java/com/github/brianmmcclain/k8sshopbackend/K8sShopBackendApplication.java
+++ apps/k8s-shop-backend/src/main/java/com/github/brianmmcclain/k8sshopbackend/K8sShopBackendApplication.java
@@ -8,7 +8,6 @@ public class K8sShopBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(K8sShopBackendApplication.class, args);
-		System.out.println("Test");
 	}
 
 }
