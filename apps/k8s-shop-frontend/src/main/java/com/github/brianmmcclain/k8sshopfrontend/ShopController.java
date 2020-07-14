4d75d5eaf807bd3f5d8d5fc4fee4813e38cfd93c
Brian McClain <brianmmcclain@gmail.com>
2020-07-14 11:36:21 -0400

    Move frontend design to thymeleaf template

--- apps/k8s-shop-frontend/src/main/java/com/github/brianmmcclain/k8sshopfrontend/ShopController.java
+++ apps/k8s-shop-frontend/src/main/java/com/github/brianmmcclain/k8sshopfrontend/ShopController.java
@@ -1,5 +1,6 @@
 package com.github.brianmmcclain.k8sshopfrontend;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
@@ -24,28 +26,13 @@ public class ShopController {
     private DiscoveryClient discoveryClient;
 
     @GetMapping("/")
-    @ResponseBody
-    public String items() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("<html><body><table style=\"border: 1px solid black\">\n");
-        sb.append("<tr>\n");
-        sb.append("<th style=\"border: 1px solid black\">Name</th>\n");
-        sb.append("<th style=\"border: 1px solid black\">Price</th>\n");
-        sb.append("<th style=\"border: 1px solid black\">Count</th>\n");
-        sb.append("</tr>\n");
-        Item[] items = getItems();
-        for (int i = 0; i < items.length; i++) {
-            sb.append("<tr>\n");
-            sb.append("<td style=\"border: 1px solid black\">").append(items[i].getName()).append("</td>\n");
-            sb.append("<td style=\"border: 1px solid black\">$").append(items[i].getPrice()).append("</td>\n");
-            sb.append("<td style=\"border: 1px solid black\">").append(items[i].getCount()).append("</td>\n");
-            sb.append("</tr>\n");
-        }
-        sb.append("</table></body></html>");
-        return sb.toString();
+    public String items(Model model) {
+        List<Item> items = getItems();
+        model.addAttribute("items", items);
+        return "index";
     }
 
-    private Item[] getItems() {
+    private List<Item> getItems() {
         RestTemplate restTemplate = new RestTemplate();
         List<ServiceInstance> instances = discoveryClient.getInstances("k8s-shop-backend-service");
         String serviceURI = "http://localhost:8083";
@@ -54,6 +41,6 @@ public class ShopController {
         }
         System.out.println("Using Service URI: " + serviceURI);
         Item[] items = restTemplate.getForObject(serviceURI + "/item", Item[].class);
-        return items;
+        return Arrays.asList(items);
     }
 }
